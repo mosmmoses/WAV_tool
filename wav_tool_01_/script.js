1b92398793dc75973f45df283631cdae5fc8ad65
mosmmoses <ed.moiseenkova@gmail.com>
2024-07-12 19:54:57 +0300

    -

--- wav_tool_01_/script.js
+++ wav_tool_01_/script.js
@@ -1,89 +1,3 @@
-// FFT definition
-class FFT {
-  constructor(bufferSize, sampleRate) {
-    this.bufferSize = bufferSize;
-    this.sampleRate = sampleRate;
-    this.spectrum = new Float32Array(bufferSize / 2);
-    this.real = new Float32Array(bufferSize);
-    this.imag = new Float32Array(bufferSize);
-    this.reverseTable = new Uint32Array(bufferSize);
-    this.sinTable = new Float32Array(bufferSize);
-    this.cosTable = new Float32Array(bufferSize);
-
-    let limit = 1;
-    let bit = bufferSize >> 1;
-
-    while (limit < bufferSize) {
-      for (let i = 0; i < limit; i++) {
-        this.reverseTable[i + limit] = this.reverseTable[i] + bit;
-      }
-      limit = limit << 1;
-      bit = bit >> 1;
-    }
-
-    for (let i = 0; i < bufferSize; i++) {
-      this.sinTable[i] = Math.sin(-Math.PI / i);
-      this.cosTable[i] = Math.cos(-Math.PI / i);
-    }
-  }
-
-  forward(buffer) {
-    const bufferSize = this.bufferSize;
-    const cosTable = this.cosTable;
-    const sinTable = this.sinTable;
-    const reverseTable = this.reverseTable;
-    const real = this.real;
-    const imag = this.imag;
-    const spectrum = this.spectrum;
-
-    if (buffer.length !== bufferSize) {
-      throw new Error('Buffer size mismatch');
-    }
-
-    for (let i = 0; i < bufferSize; i++) {
-      real[i] = buffer[reverseTable[i]];
-      imag[i] = 0;
-    }
-
-    let halfSize = 1;
-    while (halfSize < bufferSize) {
-      const phaseShiftStepReal = cosTable[halfSize];
-      const phaseShiftStepImag = sinTable[halfSize];
-      let currentPhaseShiftReal = 1.0;
-      let currentPhaseShiftImag = 0.0;
-
-      for (let fftStep = 0; fftStep < halfSize; fftStep++) {
-        let i = fftStep;
-
-        while (i < bufferSize) {
-          const off = i + halfSize;
-          const tr = (currentPhaseShiftReal * real[off]) - (currentPhaseShiftImag * imag[off]);
-          const ti = (currentPhaseShiftReal * imag[off]) + (currentPhaseShiftImag * real[off]);
-
-          real[off] = real[i] - tr;
-          imag[off] = imag[i] - ti;
-          real[i] += tr;
-          imag[i] += ti;
-
-          i += halfSize << 1;
-        }
-
-        const tmpReal = currentPhaseShiftReal;
-        currentPhaseShiftReal = (tmpReal * phaseShiftStepReal) - (currentPhaseShiftImag * phaseShiftStepImag);
-        currentPhaseShiftImag = (tmpReal * phaseShiftStepImag) + (currentPhaseShiftImag * phaseShiftStepReal);
-      }
-
-      halfSize = halfSize << 1;
-    }
-
-    for (let i = 0; i < bufferSize / 2; i++) {
-      spectrum[i] = 2 * Math.sqrt(real[i] * real[i] + imag[i] * imag[i]) / bufferSize;
-    }
-
-    return spectrum;
-  }
-}
-
 let waveformChart = null;
 let frequencyChart = null;
 let originalWaveform = null;
@@ -434,4 +348,90 @@ function jSaver(o, name, ext) {
     };
   }());
   saveByteArray([o], name + "." + ext);
+}
+
+// FFT definition
+class FFT {
+  constructor(bufferSize, sampleRate) {
+    this.bufferSize = bufferSize;
+    this.sampleRate = sampleRate;
+    this.spectrum = new Float32Array(bufferSize / 2);
+    this.real = new Float32Array(bufferSize);
+    this.imag = new Float32Array(bufferSize);
+    this.reverseTable = new Uint32Array(bufferSize);
+    this.sinTable = new Float32Array(bufferSize);
+    this.cosTable = new Float32Array(bufferSize);
+
+    let limit = 1;
+    let bit = bufferSize >> 1;
+
+    while (limit < bufferSize) {
+      for (let i = 0; i < limit; i++) {
+        this.reverseTable[i + limit] = this.reverseTable[i] + bit;
+      }
+      limit = limit << 1;
+      bit = bit >> 1;
+    }
+
+    for (let i = 0; i < bufferSize; i++) {
+      this.sinTable[i] = Math.sin(-Math.PI / i);
+      this.cosTable[i] = Math.cos(-Math.PI / i);
+    }
+  }
+
+  forward(buffer) {
+    const bufferSize = this.bufferSize;
+    const cosTable = this.cosTable;
+    const sinTable = this.sinTable;
+    const reverseTable = this.reverseTable;
+    const real = this.real;
+    const imag = this.imag;
+    const spectrum = this.spectrum;
+
+    if (buffer.length !== bufferSize) {
+      throw new Error('Buffer size mismatch');
+    }
+
+    for (let i = 0; i < bufferSize; i++) {
+      real[i] = buffer[reverseTable[i]];
+      imag[i] = 0;
+    }
+
+    let halfSize = 1;
+    while (halfSize < bufferSize) {
+      const phaseShiftStepReal = cosTable[halfSize];
+      const phaseShiftStepImag = sinTable[halfSize];
+      let currentPhaseShiftReal = 1.0;
+      let currentPhaseShiftImag = 0.0;
+
+      for (let fftStep = 0; fftStep < halfSize; fftStep++) {
+        let i = fftStep;
+
+        while (i < bufferSize) {
+          const off = i + halfSize;
+          const tr = (currentPhaseShiftReal * real[off]) - (currentPhaseShiftImag * imag[off]);
+          const ti = (currentPhaseShiftReal * imag[off]) + (currentPhaseShiftImag * real[off]);
+
+          real[off] = real[i] - tr;
+          imag[off] = imag[i] - ti;
+          real[i] += tr;
+          imag[i] += ti;
+
+          i += halfSize << 1;
+        }
+
+        const tmpReal = currentPhaseShiftReal;
+        currentPhaseShiftReal = (tmpReal * phaseShiftStepReal) - (currentPhaseShiftImag * phaseShiftStepImag);
+        currentPhaseShiftImag = (tmpReal * phaseShiftStepImag) + (currentPhaseShiftImag * phaseShiftStepReal);
+      }
+
+      halfSize = halfSize << 1;
+    }
+
+    for (let i = 0; i < bufferSize / 2; i++) {
+      spectrum[i] = 2 * Math.sqrt(real[i] * real[i] + imag[i] * imag[i]) / bufferSize;
+    }
+
+    return spectrum;
+  }
 }
